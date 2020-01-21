ea1701ef75cc9a30fa286f26d0188df1a271c2e5
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-21 14:39:01 -0600

    notification uses comment params

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,7 @@ class CommentsController < ApplicationController
     
     def create
         comment = Comment.create(comment_params)
-        create_notification(params)
+        create_notification(comment_params)
         render json: comment
     end
 
@@ -16,9 +16,9 @@ class CommentsController < ApplicationController
         params.require(:comment).permit(:user_id, :review_id, :content)
     end
 
-    def create_notification(params)
-        review = Review.find(params[:review_id])
-        actor = User.find(params[:user_id])
+    def create_notification(comment_params)
+        review = Review.find(comment_params[:review_id])
+        actor = User.find(comment_params[:user_id])
         recipient = User.find(review.user_id)
         Notification.create(recipient_id: recipient.id, actor_id: actor.id, read: false, action: 'commented', notifiable_id: review.id, notifiable_type: 'Review' )
     end
