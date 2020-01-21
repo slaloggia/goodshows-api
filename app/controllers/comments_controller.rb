30a47d32479e8e326b9cfb12924ad7993286517b
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-21 14:24:19 -0600

    notification fixes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,7 +7,10 @@ class CommentsController < ApplicationController
     
     def create
         comment = Comment.create(comment_params)
-        self.create_notification()
+        review = Review.find(params[:review_id])
+        actor = User.find(params[:user_id])
+        recipient = User.find(review.user_id)
+        Notification.create(recipient_id: recipient.id, actor_id: actor.id, read: false, action: 'commented', notifiable_id: review.id, notifiable_type: 'Review' )
         render json: comment
     end
 
@@ -16,10 +19,4 @@ class CommentsController < ApplicationController
         params.require(:comment).permit(:user_id, :review_id, :content)
     end
 
-    def create_notification
-        review = Review.find(params[:review_id])
-        actor = User.find(params[:user_id])
-        recipient = User.find(review.user_id)
-        Notification.create(recipient_id: recipient.id, actor_id: actor.id, read: false, action: 'commented', notifiable_id: review.id, notifiable_type: 'Review' )
-    end
 end
