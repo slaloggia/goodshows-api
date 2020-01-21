c40b6f90f30d93ed912078b45a5de97e4382bcf0
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-21 14:44:48 -0600

    typo in notifications controller

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,7 +2,7 @@ class NotificationsController < ApplicationController
 
     def index
         notifications = Notification.all
-        render json: notifications_controller
+        render json: notifications
     end
-    
+
 end
