6208e23388b99bd1af3c5e5207696a2458837b52
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-21 13:58:35 -0600

    moved notification creation to private method

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,15 +7,18 @@ class CommentsController < ApplicationController
     
     def create
         comment = Comment.create(comment_params)
-        review = Review.find(params[:review_id])
-        actor = User.find(params[:user_id])
-        recipient = User.find(review.user_id)
-        Notification.create(recipient_id: recipient.id, actor_id: actor.id, read: false, action: 'commented', notifiable_id: review.id, notifiable_type: 'Review' )
         render json: comment
     end
 
     private 
     def comment_params
         params.require(:comment).permit(:user_id, :review_id, :content)
+    end
+
+    def create_notification
+        review = Review.find(params[:review_id])
+        actor = User.find(params[:user_id])
+        recipient = User.find(review.user_id)
+        Notification.create(recipient_id: recipient.id, actor_id: actor.id, read: false, action: 'commented', notifiable_id: review.id, notifiable_type: 'Review' )
     end
 end
