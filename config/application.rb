f7e132c0f72ed6fd9d4ef4c9123af9c6c1603ea5
Sarah Laloggia <54509406+slaloggia@users.noreply.github.com>
2020-01-22 10:48:36 -0600

    cors all origins

--- config/application.rb
+++ config/application.rb
@@ -25,7 +25,7 @@ module GoodShowsApi
     config.load_defaults 6.0
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://good-shows.herokuapp.com/'
+        origins '*'
         resource '*',     
         headers: :any,
         methods: [:get, :post, :put, :patch, :delete, :options, :head],
